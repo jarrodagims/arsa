0b45aa187c8310a903ac54e120eef406e1b1bf4f
Jarrod Medrano <jmedran@gmail.com>
2019-03-18 16:27:19 -0600

    styling homepage

--- page-home.php
+++ page-home.php
@@ -46,4 +46,37 @@ Template Name: Home
     </div>
 </section>
 
+<section class="home-module-2">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h2>Who is 915 Siteworks?</h2>
+
+                <h3>We are a young up-and-coming company ready to take El Paso by insulation.</h3>
+
+                <p>Our main initiative is to provide El Paso homebuilders with an innovative and airtight insulation
+                    method proven to be the best performing and most eco-friendly on the market. We also provide
+                    landscaping services and dumpster rentals for the entire El Paso region.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="home-module-3">
+    <h2>Spray Foam Insulation & Landscaping</h2>
+    <h3>Services Available</h3>
+</section>
+
+<section class="home-module-4">
+    <h2>915 Siteworks helps home builders improve the quality of their homes by making them more energy efficient and
+        resistant.</h2>
+
+    <p>In today’s world, everyone is concerned about keeping energy costs down and reducing the carbon footprints. Spray
+        foam technologies improve a home’s performance, making it more efficient, and saving the owner hundreds of
+        dollars in energy bills.
+    </p>
+</section>
+
 <?php get_footer();?>
