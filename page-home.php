5cd0570d266413b43c2f92af5a0e0dc77884df46
Jarrod Medrano <jmedran@gmail.com>
2019-03-18 16:50:11 -0600

    adding modules

--- page-home.php
+++ page-home.php
@@ -70,13 +70,85 @@ Template Name: Home
 </section>
 
 <section class="home-module-4">
-    <h2>915 Siteworks helps home builders improve the quality of their homes by making them more energy efficient and
-        resistant.</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h2>915 Siteworks helps home builders improve the quality of their homes by making them more energy
+                    efficient and
+                    resistant.</h2>
 
-    <p>In today’s world, everyone is concerned about keeping energy costs down and reducing the carbon footprints. Spray
-        foam technologies improve a home’s performance, making it more efficient, and saving the owner hundreds of
-        dollars in energy bills.
-    </p>
+                <p>In today’s world, everyone is concerned about keeping energy costs down and reducing the carbon
+                    footprints. Spray
+                    foam technologies improve a home’s performance, making it more efficient, and saving the owner
+                    hundreds of
+                    dollars in energy bills.
+                </p>
+            </div>
+        </div>
+    </div>
 </section>
 
+<section class="home-module-5">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            </div>
+            <div class="col">
+                <h2>Our Location and Who We Serve</h2>
+
+                <h3>Our office is located in east El Paso at 1150 Burgundy, Suite B.
+                </h3>
+
+                <p>We currently serve and operate within a 200-mile radius of the Sun City. This means we also serve the
+                    entire El Paso region including its surrounding areas of Las Cruces, Santa Teresa, Horizon, Socorro
+                    and more!
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="home-module-6">
+    <div class="container">
+        <div class="row">
+
+            <div class="col">
+                <h2>Providing the Best and Most Effective
+                    Insulation Services Around!</h2>
+
+                <h3>Every home builder is looking to deliver the best and highest performing home for their client.
+                </h3>
+
+                <p>IWe help El Paso home builders achieve that mission by specializing and installing the most effective
+                    insulation method around. We believe in this powerful method so strongly that we offer a 10%
+                    discount to home builders on our first job. After you see how effectively we work and the difference
+                    it makes for homeowners, we’re confident you’ll never go back!
+                </p>
+
+            </div>
+
+            <div class="col">
+                <img src="<?=IMGURL ?>2745.jpg" alt="">
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<section class="home-module-7">
+    <div class="container">
+        <div class="row">
+
+            <div class="col">
+                <h2>FREE On-Site Estimates</h2>
+                <p>Did we mention we offer FREE estimates?
+                    Just give us a call and we’ll come down to the site and provide you with a free and clear estimate
+                    on our work.</p>
+
+                <a href="<?=SITEURL?>/contact-us/">CONTACT US</a>
+            </div>
+        </div>
+
+    </div>
+</section>
 <?php get_footer();?>
